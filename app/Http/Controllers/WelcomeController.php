52838d45efeabb09d3a8a5f5b0070e6066ea9af6
Rafael Lunardelli <rafael@lunardelli.me>
2024-09-26 10:50:50 -0600

    wip

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,6 +8,6 @@ class WelcomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return view('teste.jeremias');
+        return view('welcome');
     }
 }
